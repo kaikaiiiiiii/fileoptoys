f16e33506c80bd63d77fe12752970716ff403b04
kaikaiiiiiii <yourkaikai@gmail.com>
2023-01-10 18:01:17 +0800

    bug on some specific chars

--- foldering.js
+++ foldering.js
@@ -1,16 +1,6 @@
 const fs = require('fs')
 
 var targetPath = 'F:\\Anime Episodes'
-var black = [
-    `[Lilith-Raws]`,
-    `[WEB-DL]`,
-    `[1080p]`,
-    `[AVC AAC]`,
-    `[CHT]`,
-    `[MP4]`,
-    `[Baha]`,
-    `.mp4`
-]
 
 
 function escapeRegExp(string) {
@@ -20,14 +10,27 @@ function escapeRegExp(string) {
 
 // var list = fs.readdirSync(targetPath);
 var list = fs.readFileSync('ls.txt', 'utf8').split('\n');
-var filteredList = list.map(e => {
-    var s = e;
-    black.forEach(b => {
-        var reg = new RegExp(escapeRegExp(b), 'ig');
-        s = s.replace(reg, '');
-    });
-    return s.trim();
-});
 
+function lcs(a, b) {
+    console.log(a, b)
+    if (typeof a != 'string' || typeof b != 'string') { return 0 };
+    const m = a.length, n = b.length;
+    const memo = new Array(m + 1).fill().map(() => new Array(n + 1).fill(""));
+    for (let i = 1; i <= m; i++) {
+        var c1 = a[i - 1];
+        for (let j = 1; j <= m; j++) {
+            var c2 = b[j - 1];
+            if (c1 === c2) {
+                memo[i][j] = memo[i - 1][j - 1].concat(c2);
+                console.log(memo);
+            } else {
+                var t1 = memo[i - 1][j];
+                var t2 = memo[i][j - 1];
+                memo[i][j] = t1.length > t2.length ? t1 : t2;
+            }
+        }
+    }
+    return memo[m][n];
+}
 
-console.log(filteredList);
+console.log(lcs(list[1], list[2]));
