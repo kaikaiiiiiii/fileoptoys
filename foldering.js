ecc5a493d1d8fd298341e5ee77448aae8d59877b
kaikai <yourkaikai@gmail.com>
2023-01-10 21:34:17 +0800

    update

--- foldering.js
+++ foldering.js
@@ -11,11 +11,14 @@ function escapeRegExp(string) {
 // var list = fs.readdirSync(targetPath);
 var list = fs.readFileSync('ls.txt', 'utf8').split('\n');
 
+// 根据两个文件名的最小公共子序列与文件名间的相似度，判断两个文件是否可以归并到同一个目录中。
+// 两种情况：
+// 两个文件分别名为 episode 01 和 episode 02 属于同一目录。判断为相似度 > 95%
+// 两个文件分别名为 movie 和 movie extra 属于同一目录。判断为 lsc 串等于某个文件。
+
 function lcs(a, b) {
-    console.log(a, b)
     if (typeof a != 'string' || typeof b != 'string') { return 0 };
     const m = a.length, n = b.length;
-    console.log(m, n)
     const memo = new Array(m + 1).fill().map(() => new Array(n + 1).fill(""));
     for (let i = 1; i <= m; i++) {
         var c1 = a[i - 1];
@@ -32,3 +35,4 @@ function lcs(a, b) {
     }
     return memo[m][n];
 }
+
