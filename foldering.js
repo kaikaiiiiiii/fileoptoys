9b0b2ed24ac0913db43969f7d1d9fe36c0efb494
kaikaiiiiiii <yourkaikai@gmail.com>
2023-01-10 18:13:30 +0800

    bug fixed

--- foldering.js
+++ foldering.js
@@ -15,14 +15,14 @@ function lcs(a, b) {
     console.log(a, b)
     if (typeof a != 'string' || typeof b != 'string') { return 0 };
     const m = a.length, n = b.length;
+    console.log(m, n)
     const memo = new Array(m + 1).fill().map(() => new Array(n + 1).fill(""));
     for (let i = 1; i <= m; i++) {
         var c1 = a[i - 1];
-        for (let j = 1; j <= m; j++) {
+        for (let j = 1; j <= n; j++) {
             var c2 = b[j - 1];
             if (c1 === c2) {
                 memo[i][j] = memo[i - 1][j - 1].concat(c2);
-                console.log(memo);
             } else {
                 var t1 = memo[i - 1][j];
                 var t2 = memo[i][j - 1];
@@ -32,5 +32,3 @@ function lcs(a, b) {
     }
     return memo[m][n];
 }
-
-console.log(lcs(list[1], list[2]));
